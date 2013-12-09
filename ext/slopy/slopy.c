7200f0d0efee0d3f95d751b522cd5d967d225dc5
Ivan Shamatov <status.enable@gmail.com>
2013-12-09 13:18:09 +0400

    adding some checks

--- ext/slopy/slopy.c
+++ ext/slopy/slopy.c
@@ -2,27 +2,34 @@
 #include <math.h>
 
 
+// Interface
 VALUE Slopy = Qnil;
-
 void Init_slopy();
-
 VALUE method_slopy_cos(VALUE self, VALUE vector1, VALUE vector2);
+VALUE method_slopy_matrix(VALUE self, VALUE matrix);
+
+
 
 void Init_slopy() {
-  Slopy = rb_define_class("Slopy", rb_cObject);
-  rb_define_method(Slopy, "cos", method_slopy_cos, 2);
+  Slopy = rb_define_module("Slopy", rb_cObject);
+  Calculator = rb_define_module_under(Slopy, "Calculator")
+  rb_define_singleton_method(Calculator, "cos", method_slopy_cos, 2);
+  rb_define_singleton_method(Calculator, "matrix", method_slopy_matrix, 1);
 }
+
 
 
 VALUE method_slopy_cos(VALUE self, VALUE vector1, VALUE vector2) {
   int v1_length = RARRAY_LEN(vector1);
   int v2_length = RARRAY_LEN(vector2);
+  if (v1_length != v2_length) {
+    return false;
+  }
   int dot_product = 0;
   int magnitude1 = 0;
   int magnitude2 = 0;
   int v1;
   int v2;
-  
   for (int i = 0; i < v1_length; i++) {
     v1 = NUM2INT(rb_ary_entry(vector1, i));
     v2 = NUM2INT(rb_ary_entry(vector2, i));
@@ -30,9 +37,14 @@ VALUE method_slopy_cos(VALUE self, VALUE vector1, VALUE vector2) {
     magnitude1 += (v1*v1);
     magnitude2 += (v2*v2);    
   }
-  // double similarity = dotp(vector1, vector2) / (magnitude(vector1) * magnitude(vector2));
   double similarity = dot_product /(sqrt(magnitude1)* sqrt(magnitude2));
   return rb_float_new(similarity);
+}
+
+
+VALUE method_slopy_matrix(VALUE self, VALUE vectors) {
+  double matrix;
+  return matrix;
 }
 
 
